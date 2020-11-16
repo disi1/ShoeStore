2a2ca18667381e1733c38d3c40b73247ed140166
Diana Sica <diana.sica29@gmail.com>
2020-11-16 21:01:01 +0100

    added the Instructions Screen

---
+++ app/src/main/java/com/udacity/shoestore/ui/login/InstructionFragment.kt
@@ -0,0 +1,32 @@
+package com.udacity.shoestore.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentInstructionBinding
+
+class InstructionFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val binding = DataBindingUtil.inflate<FragmentInstructionBinding>(
+            inflater,
+            R.layout.fragment_instruction,
+            container,
+            false
+        )
+
+        binding.getStartedButton.setOnClickListener {
+            Toast.makeText(this.context, "Get Started clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        return binding.root
+    }
+}

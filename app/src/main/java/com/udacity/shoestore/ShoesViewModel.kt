391a62c9f93bc4e66fdb845834eacf2bd08a015b
Diana Sica <diana.sica29@gmail.com>
2020-11-17 11:13:03 +0100

    added the Activity level ViewModel which holds the list of shoes

---
+++ app/src/main/java/com/udacity/shoestore/ShoesViewModel.kt
@@ -0,0 +1,17 @@
+package com.udacity.shoestore
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.models.Shoe
+
+class ShoesViewModel: ViewModel() {
+
+    private val _shoes = MutableLiveData<List<Shoe>>(
+        mutableListOf(
+            Shoe("Unisex High Top Shoe", 23.5,"Chuck Taylor All Star", "sdfjsdfsd")
+        )
+    )
+    val shoes: LiveData<List<Shoe>>
+        get() = _shoes
+}

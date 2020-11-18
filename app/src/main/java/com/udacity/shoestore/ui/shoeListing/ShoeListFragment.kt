bdcfcba907c48dccb6a84e4b03499b340d1ea0d0
Diana Sica <diana.sica29@gmail.com>
2020-11-18 12:21:39 +0100

    created a Logout menu to return to the Login screen

--- app/src/main/java/com/udacity/shoestore/ui/shoeListing/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/shoeListing/ShoeListFragment.kt
@@ -2,16 +2,16 @@ package com.udacity.shoestore.ui.shoeListing
 
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
 import com.udacity.shoestore.ShoesViewModel
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
@@ -51,7 +51,18 @@ class ShoeListFragment : Fragment() {
             Toast.makeText(this.context, "Add Shoe button was clicked.", Toast.LENGTH_SHORT).show()
         }
 
+        setHasOptionsMenu(true)
+
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
 }

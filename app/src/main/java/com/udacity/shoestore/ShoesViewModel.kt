bd8f46552dcb228fbd320ff2d7e4aefb40fb5568
Diana Sica <diana.sica29@gmail.com>
2020-11-18 12:03:37 +0100

    created the UI for the Shoe item (to be displayed in the list of Shoes)

--- app/src/main/java/com/udacity/shoestore/ShoesViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/ShoesViewModel.kt
@@ -9,7 +9,7 @@ class ShoesViewModel: ViewModel() {
 
     private val _shoes = MutableLiveData<List<Shoe>>(
         mutableListOf(
-            Shoe("Unisex High Top Shoe", 23.5,"Chuck Taylor All Star", "Very good condition.")
+            Shoe("Converse Chuck Taylor", 25.5,"Chuck Taylor All Star", "Unisex High Top Sneakers.\n\nThe iconic innovative shoes that made headlines when announced.\n\nNever worn, in their original box, mint condition.")
         )
     )
     val shoes: LiveData<List<Shoe>>
--- app/src/main/java/com/udacity/shoestore/ui/shoeListing/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/shoeListing/ShoeListFragment.kt
@@ -1,6 +1,7 @@
 package com.udacity.shoestore.ui.shoeListing
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,11 +10,17 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.udacity.shoestore.R
+import com.udacity.shoestore.ShoesViewModel
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+import com.udacity.shoestore.databinding.ShoeItemBinding
 
 class ShoeListFragment : Fragment() {
+
+    private val shoeViewModel: ShoesViewModel by activityViewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -24,11 +31,21 @@ class ShoeListFragment : Fragment() {
             container,
             false
         )
-//
-//        (activity as AppCompatActivity).supportActionBar?.setHomeButtonEnabled(false)
-//        (activity as AppCompatActivity).supportActionBar?.setDisplayShowHomeEnabled(false)
-//        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
-//        (activity as AppCompatActivity).supportActionBar?.setHomeAsUpIndicator(null)
+
+        shoeViewModel.shoes.observe(viewLifecycleOwner, { shoeList ->
+            var objectType = "items"
+            if(shoeList.size == 1) {
+                objectType = "item"
+            }
+
+            (activity as AppCompatActivity).supportActionBar?.title = "Shoes (${shoeList.size} ${objectType})"
+
+            shoeList.forEach {
+                val itemBinding = DataBindingUtil.inflate<ShoeItemBinding>(inflater, R.layout.shoe_item, container,false)
+                itemBinding.shoe = it
+                binding.shoeItemsLinearLayout.addView(itemBinding.root)
+            }
+        })
 
         binding.addShoeButton.setOnClickListener {
             Toast.makeText(this.context, "Add Shoe button was clicked.", Toast.LENGTH_SHORT).show()
